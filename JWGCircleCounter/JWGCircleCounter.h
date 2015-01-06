db56a21cc89fdf220348d0c52ad500eecf0dabcc
John Graham <johngraham262@gmail.com>
2015-01-06 00:07:56 -0500

    Bump version to 0.2.2

--- JWGCircleCounter/JWGCircleCounter.h
+++ JWGCircleCounter/JWGCircleCounter.h
@@ -1,6 +1,6 @@
 //
 //  JWGCircleCounter.h
-//  Version 0.2.1
+//  Version 0.2.2
 //
 //  https://github.com/johngraham262/JWGCircleCounter
 //
--- JWGCircleCounterTests/JWGCircleCounterTests.m
+++ JWGCircleCounterTests/JWGCircleCounterTests.m
@@ -1,6 +1,6 @@
 //
 //  JWGCircleCounterTests.m
-//  Version 0.2.1
+//  Version 0.2.2
 //
 //  https://github.com/johngraham262/JWGCircleCounter
 //
